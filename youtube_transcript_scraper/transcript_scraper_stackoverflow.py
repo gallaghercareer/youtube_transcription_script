e29b48ac44a90d7de5cb25858b14a8d53a588b22
John Gallagher <gallaghercareer@gmail.com>
2023-03-09 10:26:03 -0500

    update

--- youtube_transcript_scraper/transcript_scraper_stackoverflow.py
+++ youtube_transcript_scraper/transcript_scraper_stackoverflow.py
@@ -19,31 +19,27 @@ def read_api_key(input_file):
     with open(input_file, 'r') as file:
         # Read the contents of the file as a string
         string_variable = file.read()
-
-    return string_variable
+        return string_variable
 
 # Define a function to split a list into chunks of a given size
 def chunks(lst, size):
     return [lst[i:i+size] for i in range(0, len(lst), size)]
 
-
+counter = 0
 #openai_key
 openai.api_key = read_api_key('config.txt')
 #pinecone_key
 pinecone_key = read_api_key('pinecone_config.txt')
 
 #Connect to Pinecone API
-pinecone.init(
-    api_key=pinecone_key,
-    environment = 'us-east1-gcp'
-)
+pinecone.init(api_key=pinecone_key,environment = 'us-east1-gcp')
 
 #check if index database already exists (only create index if it doesn't)
 if 'index-test' not in pinecone.list_indexes():
-    print('vector database created')
+    # print('vector database created')
     pinecone.create_index('index-test',dimension=len(embeds[0]))
-else:
-    print("vector database already exists")
+    #else:
+    #print("vector database already exists")
 
 #assign pinecone Index object
 index = pinecone.Index('index-test')
@@ -51,44 +47,48 @@ hasVectors = None
 
 #check Index object stats (vector count)
 indexStats = index.describe_index_stats()
-print(indexStats)
+#print(indexStats)
 
 
 #assign vector count to variable
 num_vectors = indexStats.total_vector_count
-print(num_vectors)
-if num_vectors > 0 :
-    print(f"The 'my-index' index contains {num_vectors} vectors.")
-    hasVectors = True
+#print(num_vectors)
+if num_vectors > 0 :   
+    # print(f"The 'my-index' index contains {num_vectors} vectors.")
+    hasVectors = True   
 else:
-    print(f"The index does not contain any vectors")
+    # print(f"The index does not contain any vectors")
     hasVectors = False
 
 #declare ID for all transcript snippets in Vector Database
 vector_batch_id = 0
 
-
 #####SCRAPE YOUTUBE########
 #create a collection of video objects containing the VideoID
-videos = scrapetube.get_channel("UCQVOeT4i5nezvPG_xvIFzdQ",limit=10)
+videos = scrapetube.get_channel("UCv83tO5cePwHMt1952IVVHw", limit=2)
 
-#Declare a variable to hold only the videoIDs
+#Declare a vaiable to hold only the videoIDs
 videoId_list = []
 
-#iterate over collection of video objects to isolate the videoIDs
-for i in tqdm(range(0, len(videos), 1)):
+#declare vector total for increment
+vectorTotal = 0
 
-    for video in videos:
+#vector list for every embedding
+vector_list = []
+
+for video in videos:
     #create list of video ids    
     videoId_list.append(video['videoId'])   
 
-
-    all_channel_transcripts = []
     joined_sentences = []
+
+    total_number_of_videos = len(videoId_list)
+    #print(total_number_of_videos)
+
+for i in tqdm(range(0, total_number_of_videos, 1)):
     
     #iterate over the collection of video IDs and collect the transcripts 
     for videoId in videoId_list:
-
         #call the youtube api to return a video transcript object
         transcript = YouTubeTranscriptApi.get_transcript(videoId)
         #pprint(transcript)
@@ -99,45 +99,49 @@ for i in tqdm(range(0, len(videos), 1)):
         #create a nested list with 5 sentence elements in each nested list
         chunks_of_five= chunks(sentences,5)
         sentence_time = chunks(sentence_time,5)
-        
-        
         #combine the 5 sentences in each nested list so that there is no nested list
         joined_sentences = [' '.join(chunk) for chunk in chunks_of_five]
+
+        ############BEGIN EMBEDDING PROCESS###################################################################
+        #thoughts:
+        #we have already 'batched' our sentences into 5 sentences for each element, however 
+        #the number of batches is unknown! We must use a range...we cannot add all loops up into one upsert.
+        #we need to upload the embedded sentences, corresponding ID, and corresponding metadata
+
+        for j in joined_sentences:
+            #process everything in batches of 1
+
+            #Call OpenAI embedding api and input the chunk of text        
+            res = openai.Embedding.create(input=j, engine='text-embedding-ada-002')
+
+            #Assign the embedded data to the embeds variable
+            embeds = [record['embedding'] for record in res['data']]
+
+            #prep metadata and upsert batch to Pinecone
+
+            vectors = [{
+            'id': counter,  # Set the ID key to an incremental value
+            'values': res['data'][0]['embedding'],  # Set the values key to the embedded text
+            'meta': {
+            'url': 'https://www.youtube.com/watch?v=' + videoId,
+            'text': j
+            }
+            }]
+
+            vector_list.append(vectors)
+            counter = counter + 1
         
-        
-    ####BEGIN EMBEDDING PROCESS###################################################################
-    
-        #unique IDs
-        count = 0  
+batch_size = 100
 
-        #process everything in batches of 1
-        batch_size = 1  
+# Split the vectors into batches of size batch_size
+vector_batches = [vector_list[i:i+batch_size] for i in range(0, len(vector_list), batch_size)]
 
-        # set end position of batch
-        i_end = min(i+batch_size, len(joined_sentences))
+print(vector_batches[0])
+#vector_batches_zipped = zip(vector_batches)
+# Upsert each batch to Pinecone
+for vb in vector_batches:
+    vb_zipped = zip(vb)
+    index.upsert(vectors=list(vb_zipped))
 
-        # get batch of lines and IDs
-        lines_batch = joined_sentences[i: i+batch_size]
-        #print(lines_batch)
-        
-        vector_batch_id += 1
-        #pprint(ids_batch)
+print("PINECONE UPSERT COMPLETE")
 
-        #Call OpenAI embedding api and input the chunk of text        
-        res = openai.Embedding.create(input=lines_batch, engine='text-embedding-ada-002')
-
-        #Assign the embedded data to the embeds variable
-        embeds = [record['embedding'] for record in res['data']]
-
-        #prep metadata and upsert batch to Pinecone
-        meta = [{'text': line, 'url':'https://www.youtube.com/watch?v=' + videoId} for line in lines_batch]
-
-        #time =[{'time': subtime[0]} for subtime in sentence_time]
-
-        to_upsert = zip(vector_batch_id, embeds, meta)
-
-        # upsert to Pinecone
-        index.upsert(vectors=list(to_upsert))
-        print("PINECONE UPSERT COMPLETE"
-
-    print("COMPLETE!!")
